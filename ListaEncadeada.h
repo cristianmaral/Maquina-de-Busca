b616b35d9e3bbbcede36ee08d40e2b1d9028bac6
cristianmaral <cristianamaralsilva@hotmail.com>
2017-04-25 21:40:09 -0300

    Create ListaEncadeada.h

---
+++ ListaEncadeada.h
@@ -0,0 +1,25 @@
+#ifndef LISTAENCADEADA_H_INCLUDED
+#define LISTAENCADEADA_H_INCLUDED
+
+typedef struct {
+    int qtde;
+    int idDoc;
+    float relevancia;
+    float peso;
+} TItem;
+
+typedef struct Celula {
+    TItem item;
+    struct Celula *prox;
+} TCelula;
+
+typedef struct {
+    TCelula *primeiro;
+    TCelula *ultimo;
+} TLista;
+
+void inicializaLista (TLista *Lista);
+void insereLista (TLista *Lista, TItem *item);
+void imprimeLista(TLista *Lista);
+
+#endif // LISTAENCADEADA_H_INCLUDED
