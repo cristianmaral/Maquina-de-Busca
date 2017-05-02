e48dea6052886d2d5a96ecfeb21b62446fc2e575
cristianmaral <cristianamaralsilva@hotmail.com>
2017-05-02 20:47:32 -0300

    Update Patricia.h

--- Patricia.h
+++ Patricia.h
@@ -3,9 +3,6 @@
 
 #include "ListaEncadeada.h"
 
-#define TRUE 1
-#define FALSE 0
-
 typedef enum {
   Interno, Externo
 } TipoNo;
@@ -16,28 +13,27 @@ typedef struct TipoPatNo {
     union {
         /* Estrutura de um nó interno */
         struct {
-            short Index;
-            char Caractere;
+            unsigned int Index; /* Índice do nó interno */
+            char Caractere; /* Caractere do nó interno */
             struct TipoPatNo *Esq, *Dir; /* Apontadores para o filho à esquerda e o filho à direita, respectivamente */
         } NInterno;
-        char Chave[50]; /* Palavra do nó externo */
+        /* Estrutura de um nó interno */
+        struct {
+            char Palavra[50]; /* Palavra do nó externo */
+            TLista Lista; /* Cada nó externo possui uma lista encadeada */
+        } NExterno;
     } NO;
-    /* Cada nó externo possui uma lista encadeada */
-    struct {
-        TLista Lista;
-        int tamanho;
-    } NoLista;
+
 } TipoPatNo;
 
 void inicializaPatricia (TipoPatNo **raiz);
-char Caractere (short i, char *k);
-unsigned int MenorIgual (char a, char b);
+unsigned int ComparaChar (char a, char b);
 unsigned int ConfereTipoNo (TipoPatNo *no);
 /* Funções para criação de nós da árvore Patricia */
 TipoPatNo* CriaNoInt (short i, TipoPatNo **Esq,  TipoPatNo **Dir, char Caractere);
 TipoPatNo* CriaNoExt (char *k, int idDoc);
-/* Função de pesquisa */
-void pesquisaPatricia (char *k, TipoPatNo *t, int idDoc);
+
+void PesquisaPatricia (char *k, TipoPatNo *t, int idDoc);
 
 /* Funções para inserir uma palavra na árvore Patricia */
 TipoPatNo* InsereEntre (char *k, TipoPatNo **t, short i, int idDoc);
@@ -46,4 +42,3 @@ TipoPatNo* InserePatricia (char *k, TipoPatNo **t, int idDoc); /* A função Ins
 void imprimePatricia (TipoPatNo *no);
 
 #endif // PATRICIA_H_INCLUDED
-
