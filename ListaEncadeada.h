86081708f5ca1f871ca7d38bb1e6fada55ea6287
cristianmaral <cristianamaralsilva@hotmail.com>
2017-05-04 22:32:13 -0300

    Update ListaEncadeada.h

--- ListaEncadeada.h
+++ ListaEncadeada.h
@@ -1,21 +1,38 @@
 #ifndef LISTAENCADEADA_H_INCLUDED
 #define LISTAENCADEADA_H_INCLUDED
 
+#include <stdio.h>
+
+/* Estrutura de um item - Utilizamos union para diferenciar um item da árvore Patricia de um item da Lista de Arquivos */
 typedef struct {
-    int qtde;
-    int idDoc;
-    float peso;
+    union {
+        /* Estrutura de um item - Nó externo Patricia */
+        struct {
+            int qtde; /* Quantidade de ocorrências de um termo */
+            int idDoc; /* Identificação do documento */
+            float peso; /* Peso do termo */
+        } termo;
+
+        /* Estrutura de um item - Lista de Arquivos */
+        struct {
+            /* Variáveis quando estamos tratando de uma Lista de Arquivos */
+            FILE *entrada; /* Variável utilizada apenas quando estamos tratando de uma Lista de Arquivos */
+            char nome_arquivo[50]; /* String utilizada para guardar o nome do arquivo */
+        } arq;
+    } it;
 } TItem;
 
+/* Estrutura de uma célula */
 typedef struct Celula {
-    TItem item;
-    struct Celula *prox;
+    TItem item; /* Um item */
+    struct Celula *prox; /* Ponteiro para a próxima célula */
 } TCelula;
 
+/* Estrutura da Lista Encadeada */
 typedef struct {
-    TCelula *primeiro;
-    TCelula *ultimo;
-    int tamanho;
+    TCelula *primeiro; /* Ponteiro para a célula cabeça  */
+    TCelula *ultimo; /* Ponteiro para a última célula da lista */
+    int tamanho; /* Tamanho da lista */
 } TLista;
 
 void inicializaLista (TLista *Lista);
