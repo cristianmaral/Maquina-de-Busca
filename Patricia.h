2551b1df3ef288f4694611479875376ab713704c
cristianmaral <cristianamaralsilva@hotmail.com>
2017-05-05 12:26:12 -0300

    Update Patricia.h

--- Patricia.h
+++ Patricia.h
@@ -1,47 +1,15 @@
-#ifndef PATRICIA_H_INCLUDED
-#define PATRICIA_H_INCLUDED
+#ifndef MAQUINADEBUSCA_H_INCLUDED
+#define MAQUINADEBUSCA_H_INCLUDED
 
-#include "ListaEncadeada.h"
+#include "Patricia.h"
+#include "TST.h"
+#include <stdio.h>
 
-typedef enum {
-  Interno, Externo
-} TipoNo;
+char *strlwr (char *str);
+void AdicionaArquivo (TLista *ListaArquivos);
+void FechaArquivos (TLista *ListaArquivos);
+void CalculaPesos (TipoPatNo *t, int N);
+void CalculaTermosDistintos (TipoPatNo *no, TCelula *arq, int idDoc);
+void MontaIndiceInvertido (TLista *ListaArquivos);
 
-/* Estrutura de um nó da árvore Patricia */
-typedef struct TipoPatNo {
-    TipoNo nt; /* Interno ou Externo */
-    union {
-        /* Estrutura de um nó interno */
-        struct {
-            unsigned int Index; /* Índice do nó interno */
-            char Caractere; /* Caractere do nó interno */
-            struct TipoPatNo *Esq, *Dir; /* Apontadores para o filho à esquerda e o filho à direita, respectivamente */
-        } NInterno;
-        /* Estrutura de um nó interno */
-        struct {
-            char Palavra[50]; /* Palavra do nó externo */
-            TLista Lista; /* Cada nó externo possui uma lista encadeada */
-        } NExterno;
-    } NO;
-
-} TipoPatNo;
-
-void inicializaPatricia (TipoPatNo **raiz);
-short ComparaChar (char a, char b);
-short ConfereTipoNo (TipoPatNo *no);
-/* Funções para criação de nós da árvore Patricia */
-TipoPatNo* CriaNoInt (short i, TipoPatNo **Esq,  TipoPatNo **Dir, char  Caractere);
-TipoPatNo* CriaNoExt (char *k, int idDoc);
-
-void PesquisaPatricia (char *k, TipoPatNo *t, int idDoc);
-
-/* Funções para inserir uma palavra na árvore Patricia */
-TipoPatNo* InsereEntre (char *k, TipoPatNo **t, short i, int idDoc, char Caractere);
-TipoPatNo* InserePatricia (char *k, TipoPatNo **t, int idDoc); /* A função Insere encapsula a função InsereEntre */
-
-void imprimePatricia (TipoPatNo *no);
-
-/* Funções da Máquina de Buscas */
-void CalculaPeso (TipoPatNo *t, int N);
-
-#endif // PATRICIA_H_INCLUDED
+#endif // MAQUINADEBUSCA_H_INCLUDED
