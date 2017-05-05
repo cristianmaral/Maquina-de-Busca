29aacc302d63d214ca260e456d91357f17b982fc
cristianmaral <cristianamaralsilva@hotmail.com>
2017-05-05 12:29:53 -0300

    Update MaquinaDeBusca.h

--- MaquinaDeBusca.h
+++ MaquinaDeBusca.h
@@ -3,11 +3,14 @@
 
 #include "Patricia.h"
 #include "TST.h"
-#include <stdio.h>
 
 char *strlwr (char *str);
 void AdicionaArquivo (TLista *ListaArquivos);
 void FechaArquivos (TLista *ListaArquivos);
+
+void CalculaPesos (TipoPatNo *t, int N);
+void CalculaTermosDistintos (TipoPatNo *no, TCelula *arq, int idDoc);
+
 void MontaIndiceInvertido (TLista *ListaArquivos);
 
 #endif // MAQUINADEBUSCA_H_INCLUDED
