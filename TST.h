3f566214041af30551c8189f9296b7cff548d727
cristianmaral <cristianamaralsilva@hotmail.com>
2017-05-08 20:14:29 -0300

    Versão Final

--- TST.h
+++ TST.h
@@ -4,23 +4,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXTAM 50 /* Tamanho m·ximo de letras que uma palavra pode conter */
+#define MAXTAM 50 /* Tamanho m√°ximo de letras que uma palavra pode conter */
 
-/* NÛ da ¡rvore TST */
+/* N√≥ da √Årvore TST */
 typedef struct TipoTSTNo
 {
     char caractere;
-    unsigned fimString; /* Verdadeiro se esse caractere for o ˙ltimo caractere de uma das palavras */
+    unsigned fimString; /* Verdadeiro se esse caractere for o √∫ltimo caractere de uma das palavras */
     struct TipoTSTNo *esq, *meio, *dir; /* Apontadores para o filho da esquerda, do meio e da direita, respectivamente */
 } TipoTSTNo;
-/* FunÁıes TST */
+
 void inicializaTST (TipoTSTNo **raiz);
 TipoTSTNo * novoNoTST (char data);
 void insereTST (TipoTSTNo** no, char *palavra);
 void percorre (TipoTSTNo* no, char* palavra, char* prefixo, int indice,TLista *retorno);
 void percorreTST (TipoTSTNo* raiz, char *prefixo,TLista *retorno);
-int pesquisaTST (TipoTSTNo *no, char *palavra);
-/* FunÁ„o da ImplementaÁ„o AutoPreenchimento */
-void AutoPreenchimentoTST(TipoTSTNo *no, int indice, char *prefixo,TLista *retorno);
 
 #endif // TST_H_INCLUDED
