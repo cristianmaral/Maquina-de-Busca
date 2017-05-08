0f881a4226c54aaa22617986597dea4cb54bd204
cristianmaral <cristianamaralsilva@hotmail.com>
2017-05-08 20:19:40 -0300

    Versão Final

---
+++ ListaEncadeadaDeArquivos.h
@@ -0,0 +1,9 @@
+#ifndef LISTAENCADEADADEARQUIVOS_H_INCLUDED
+#define LISTAENCADEADADEARQUIVOS_H_INCLUDED
+
+#include "ListaEncadeada.h"
+
+void OrdenaListaArquivos (TLista *ListaArquivos);
+void ReordenaListaArquivos (TLista *ListaArquivos);
+
+#endif // LISTAENCADEADADEARQUIVOS_H_INCLUDED
