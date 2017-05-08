d7ef413309a5f62acee22db546209b0fbc4e1229
cristianmaral <cristianamaralsilva@hotmail.com>
2017-05-08 20:12:43 -0300

    Versão Final

--- MaquinaDeBusca.h
+++ MaquinaDeBusca.h
@@ -3,22 +3,23 @@
 
 #include "Patricia.h"
 #include "TST.h"
+#include "ListaEncadeadaDeArquivos.h"
 #include <gtk/gtk.h>
-TLista *ListaArquivos;
+
+TLista ListaArquivos;
 TipoPatNo *raizPat;
 TipoTSTNo *raizTST;
 gboolean cancela;
 
-char *strlwr (char *str);
+char* strlower (char *str);
 void RetiraArquivo (char *nome_arq);
 void AdicionaArquivo (char* nome_arq);
 void FechaArquivos ();
 void CalculaPesos (TipoPatNo *t, int N);
+float RetornaPesoTermo (char *k, TipoPatNo *t, int idDoc);
 void CalculaTermosDistintos (TipoPatNo *no, TCelula *arq, int idDoc);
 void CalculaRelevancia (TipoPatNo *no, char **palavras, int n_termos);
 void MontaIndiceInvertido();
 void BuscaTermos (TipoPatNo *no, char *string);
-
-
 
 #endif // MAQUINADEBUSCA_H_INCLUDED
